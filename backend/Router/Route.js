0f8ac2933773b40189b2d13bee1a33f5bc7ddaee
DaYroXy <tmureckless@gmail.com>
2024-04-17 22:13:34 +0300

    backend

--- backend/Router/Route.js
+++ backend/Router/Route.js
@@ -2,8 +2,19 @@ const express = require("express");
 const router = express.Router();
 
 // Routes here
-router.get("/api", (req, res) => {
+router.get("/", (req, res) => {
     res.send("hi")
+})
+
+router.get("/api/users", (req, res) => {
+    const data = [
+        { username: "Anas", password: "123" },
+        { username: "karim", password: "123" },
+        { username: "fb5i", password: "123" },
+        { username: "2ere", password: "123" },
+    ]
+
+    res.json(data)
 })
 
 module.exports = router;
